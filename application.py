5b5d700975dba29a047138322f2296810abe84ea
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-22 13:36:19 -0600

    This is the High number

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-# Aqui escribe tu codigo
-
-print "Hello World"
-
+print "Welcome\n"
 from random import randrange
-def random_number():
-	ran_num = randrange(1,20)
+conteo = 0
 
+def aleatorio():
+	numero = input("Insert a number\n")
+	ran_num = randrange(1,20)
+	if  numero > ran_num :
+		print "You guess to high please try again"
+#while conteo < 4:
+
+#conteo = conteo + 1
+
+aleatorio()
+	
+	
