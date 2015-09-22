778ed72d41822cf7500c74b0375e6c9182d5a920
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-22 14:59:43 -0600

    We finish the tree stories

--- application.py
+++ application.py
@@ -1,3 +1,17 @@
-# Aqui escribe tu codigo
-
 print "Hello World"
+from random import randrange
+conteo = 0
+
+def aleatorio():
+	numero = input("Insert a number\n")
+	ran_num = randrange(1,20)
+	if  numero > ran_num :
+		print "You guess to high please try again"
+#while conteo < 4:
+
+#conteo = conteo + 1
+
+aleatorio()
+	
+	
+			
