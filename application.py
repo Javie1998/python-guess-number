08d063bda0088145c1bb9ec667d21e8ad2e2b657
Javie1998 <floresjavier251@gmail.com>
2015-09-22 14:46:18 -0600

    documentacion de la aplicacion previo a subirla a la master

--- application.py
+++ application.py
@@ -1,19 +1,16 @@
 print "Welcome\n"
-from random import randrange
-count = 0
-"""number = ""
-ran_num"""
-def aleatorio():
-	number = input("Insert a number\n")
-	ran_num = randrange(1,20)
-	if  number > ran_num :
-		print "You guess to high please try again"
-	elif number < ran_num:
-		print "you guess too low, please try again"
-	#elif num == incognito:
+from random import randrange #importa "randrange" de random
 
-#conteo = conteo + 1
+number = input("Insert a number\n") #pide al usuario que ingrese un numero 
+ran_num = randrange(1,20) #genera un numero aleatorio del 1 al 20
+	
+	#verifica si el numero ingresado es mayor o menor que el generado
+if  number > ran_num : 
+	print "You guess to high please try again"
+elif number < ran_num:
+	print "you guess too low, please try again"
 
-aleatorio()
+
+
 	
 	
