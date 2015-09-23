727af1caa69b19694c87682f623675a2e6c92e02
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-23 15:05:08 -0600

    This is the fianl app

--- application.py
+++ application.py
@@ -8,28 +8,30 @@ COUNT = 0
 while COUNT < 4:
     print "This is your "+str(COUNT +1) + " turn"
     RAN_NUM = randrange(1, 21)
-    NUMBER = int(raw_input("Insert a number from 1 to 20\n"))
-    if NUMBER > RAN_NUM:
-        print "You guess to high, please try again\n"
-    elif NUMBER < RAN_NUM:
-        print "you guess too low, please try again\n"
-    elif NUMBER == RAN_NUM:
-        print "YOU WIN\n"
-        ANSWER = raw_input("Do you want to play again y/n?\n")
-        ANSWER.lower()
-        if ANSWER == "y" or ANSWER == "yes":
-            COUNT = 0
-            os.system("cls")
-        elif ANSWER == "n" or ANSWER == "no":
-            sys.exit()
-    COUNT += 1
-    if COUNT == 4:
-        print "GAME OVER"
-        ANSWER = raw_input("Do you want to play again y/n?\n")
-        ANSWER.lower()
-        if ANSWER == "y" or ANSWER == "yes":
-            COUNT = 0
-            os.system("cls")
-        elif ANSWER == "n" or ANSWER == "no":
-            sys.exit()
-            
+    try:
+        NUMBER = int(raw_input("Insert a number from 1 to 20\n"))
+        if NUMBER > RAN_NUM:
+            print "You guess to high, please try again\n"
+        elif NUMBER < RAN_NUM:
+            print "you guess too low, please try again\n"
+        elif NUMBER == RAN_NUM:
+            print "YOU WIN\n"
+            ANSWER = raw_input("Do you want to play again y/n?\n")
+            ANSWER.lower()
+            if ANSWER == "y" or ANSWER == "yes":
+                COUNT = 0
+                os.system("cls")
+            elif ANSWER == "n" or ANSWER == "no":
+                sys.exit()
+        COUNT += 1
+        if COUNT == 4:
+            print "GAME OVER"
+            ANSWER = raw_input("Do you want to play again y/n?\n")
+            ANSWER.lower()
+            if ANSWER == "y" or ANSWER == "yes":
+                COUNT = 0
+                os.system("cls")
+            elif ANSWER == "n" or ANSWER == "no":
+                sys.exit()
+    except ValueError:
+        print "Insert only integer numbers"
