3f63183b4c4a87d589a472375d159980353f03f6
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-23 10:39:28 -0600

    Here we make de first testing

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
+"""This is an application that generate a random number"""
 print "Welcome\n"
-from random import randrange 
-number = input("Insert a number\n")  
-ran_num = randrange(1,20) 
-if  number > ran_num : 
-	print "You guess to high please try again"
-elif number < ran_num:
-	print "you guess too low, please try again"
+
+from random import randrange
+NUMBER = int(raw_input("Insert a number\n"))
+RAN_NUM = randrange(1, 20)
+if NUMBER > RAN_NUM:
+    print "You guess to high please try again"
+elif NUMBER < RAN_NUM:
+    print "you guess too low, please try again"
+    
