87b6538402194bf6a1784f7c069e52e59751f85d
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-23 13:51:24 -0600

    Here we show a mesage that say You Win

--- application.py
+++ application.py
@@ -2,10 +2,14 @@
 print "Welcome\n"
 
 from random import randrange
-NUMBER = int(raw_input("Insert a number\n"))
 RAN_NUM = randrange(1, 21)
+COUNT = 0
+NUMBER = int(raw_input("Insert a number\n"))
 if NUMBER > RAN_NUM:
-    print "You guess to high please try again"
+    print "You guess to high, please try again\n"
 elif NUMBER < RAN_NUM:
-    print "you guess too low, please try again"
+    print "you guess too low, please try again\n"
+elif NUMBER == RAN_NUM:#if the number is equals to the random number it will show "you win"
+    print "YOU WIN\n"
+   
     
