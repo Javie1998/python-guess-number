92c29fe763eead34a12436931fe8e6c411bc6300
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-24 15:11:47 -0600

    Guess a Number

--- application.py
+++ application.py
@@ -1,14 +1,32 @@
 """This is an application that generate a random number"""
-print 
+print """                                                                                                                                                                  
+                                                                                                   ,--.                                                           
+  ,----..                                                             ,---,                      ,--.'|                       ____                                
+ /   /   \                                                           '  .' \                 ,--,:  : |                     ,'  , `.  ,---,                       
+|   :     :          ,--,                                           /  ;    '.            ,`--.'`|  ' :         ,--,     ,-+-,.' _ |,---.'|               __  ,-. 
+.   |  ;. /        ,'_ /|             .--.--.    .--.--.           :  :       \           |   :  :  | |       ,'_ /|  ,-+-. ;   , |||   | :             ,' ,'/ /| 
+.   ; /--`    .--. |  | :    ,---.   /  /    '  /  /    '          :  |   /\   \          :   |   \ | :  .--. |  | : ,--.'|'   |  ||:   : :      ,---.  '  | |' | 
+;   | ;  __ ,'_ /| :  . |   /     \ |  :  /`./ |  :  /`./          |  :  ' ;.   :         |   : '  '; |,'_ /| :  . ||   |  ,', |  |,:     |,-.  /     \ |  |   ,' 
+|   : |.' .'|  ' | |  . .  /    /  ||  :  ;_   |  :  ;_            |  |  ;/  \   \        '   ' ;.    ;|  ' | |  . .|   | /  | |--' |   : '  | /    /  |'  :  /   
+.   | '_.' :|  | ' |  | | .    ' / | \  \    `. \  \    `.         '  :  | \  \ ,'        |   | | \   ||  | ' |  | ||   : |  | ,    |   |  / :.    ' / ||  | '    
+'   ; : \  |:  | : ;  ; | '   ;   /|  `----.   \ `----.   \        |  |  '  '--'          '   : |  ; .':  | : ;  ; ||   : |  |/     '   : |: |'   ;   /|;  : |    
+'   | '/  .''  :  `--'   \'   |  / | /  /`--'  //  /`--'  /        |  :  :                |   | '`--'  '  :  `--'   \   | |`-'      |   | '/ :'   |  / ||  , ;    
+|   :    /  :  ,      .-./|   :    |'--'.     /'--'.     /         |  | ,'                '   : |      :  ,      .-./   ;/          |   :    ||   :    | ---'     
+ \   \ .'    `--`----'     \   \  /   `--'---'   `--'---'          `--''                  ;   |.'       `--`----'   '---'           /    \  /  \   \  /           
+  `---`                     `----'                                                        '---'                                     `-'----'    `----'            
+                                                                                                                                                                  """
 
-
-
-
-
-
-
-
-print "Welcome\n"
+print """
+                            =====================================================
+                            |--------------------Welcome------------------------|
+                            |                                                   |
+                            |----- Hi, in this game we gonna challenge you------|
+                            |--------to Guess a number. Do you accept?----------|
+                            |------ You have to use only Integer Numbers--------|
+                            |-------------You only have 4 turns-----------------|
+                            |-----------------Enjoy it!! n.n--------------------|
+                            =====================================================        
+                                     \n"""
 import sys
 import os
 from random import randrange
@@ -18,22 +36,23 @@ COUNT = 0
 while COUNT < 4:
     if COUNT == 0:
         RAN_NUM = randrange(1, 21)
+
     print "This is your turn number: "+str(COUNT +1)
     try:
         NUMBER = int(raw_input("Insert a number from 1 to 20\n"))
         if NUMBER > RAN_NUM:
-            print "You guess to high, please try again\n"
+            print """***You guess to high, please try again***\n"""
         elif NUMBER < RAN_NUM:
-            print "you guess too low, please try again\n"
+            print """***you guess too low, please try again***\n"""
         elif NUMBER == RAN_NUM:
-            print "YOU WIN\n"
+            print "YOU WIN $.$\n"
             COUNT = 3
         elif NUMBER < RAN_NUM or NUMBER > RAN_NUM and COUNT == 4:
-            print "Game OVER "
+            print """Game OVER :[ """
         COUNT += 1
         try:
             if COUNT == 4:
-                print "GAME OVER"
+                print "GAME OVER "
                 QUEST = True
                 while QUEST == True:
                     ANSWER = raw_input("Do you want to play again y/n?\n")
@@ -43,9 +62,16 @@ while COUNT < 4:
                         QUEST = False
                         os.system("cls")
                         os.system("clear")
+                        print """
+                        =============================================================
+                        |------------------Hi, Welcome again n.n--------------------|
+                        |------In this time we give to you a second chance----------|
+                        |-----to win!, Do you think you can do it this time??-------|
+                        =============================================================
+                        """
                     elif ANSWER == "n" or ANSWER == "no":
                         QUEST = False
-                        print "Thanks for play, come back soon\n"
+                        print "Thanks for play, come back soon, We hope to see you again!! *--*\n"
                         raw_input("Press enter")
                         sys.exit()
 
