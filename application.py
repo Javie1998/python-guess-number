00e1363cac2e6aa98e4b3c2e80c11509d3b96b27
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-24 10:15:29 -0600

    Finaly we finish!

--- application.py
+++ application.py
@@ -3,38 +3,39 @@ print "Welcome\n"
 import sys
 import os
 from random import randrange
-
 COUNT = 0
+RAN_NUM = randrange(1, 21)
 while COUNT < 4:
-    print "This is your "+str(COUNT +1) + " turn"
-    RAN_NUM = randrange(1, 21)
+    print "This is your turn number: "+str(COUNT +1)
     try:
         NUMBER = int(raw_input("Insert a number from 1 to 20\n"))
         if NUMBER > RAN_NUM:
             print "You guess to high, please try again\n"
         elif NUMBER < RAN_NUM:
             print "you guess too low, please try again\n"
-        elif (NUMBER == RAN_NUM) and (ANSWER.isalpha() == True):
+        elif NUMBER == RAN_NUM:
             print "YOU WIN\n"
-            ANSWER = raw_input("Do you want to play again y/n?\n")
-            ANSWER.lower()
-            if ANSWER == "y" or ANSWER == "yes":
-                COUNT = 0
-                os.system("cls")
-            elif ANSWER == "n" or ANSWER == "no":
-                ANSWER.isalpha()
-                sys.exit()
+            COUNT = 3
+        elif NUMBER < RAN_NUM or NUMBER > RAN_NUM and COUNT == 4:
+            print "Game OVER "
         COUNT += 1
         try:
-            if (COUNT == 4) and (ANSWER.isalpha() == True):
+            if COUNT == 4:
                 print "GAME OVER"
-                ANSWER = raw_input("Do you want to play again y/n?\n")
-                ANSWER.lower()
-                if ANSWER == "y" or ANSWER == "yes":
-                    COUNT = 0
-                    os.system("cls")
-                elif ANSWER == "n" or ANSWER == "no":    
-                    sys.exit()
+                QUEST = True
+                while QUEST == True:
+                    ANSWER = raw_input("Do you want to play again y/n?\n")
+                    ANSWER.lower()
+                    ANSWER.isalpha()
+                    if ANSWER == "y" or ANSWER == "yes":
+                        COUNT = 0
+                        QUEST = False
+                        os.system("cls")
+                    elif ANSWER == "n" or ANSWER == "no":
+                        QUEST = False
+                        sys.exit()
+                    else:
+                        print "Insert only y/n\n"
         except ValueError:
             print "Insert only y or n\n"
     except ValueError:
